17f3448daf45bd90f8834f6e6e4f7961ba8862c6
Reuben Armstrong <reuben@reubarm.com>
2022-06-11 22:15:43 +0100

    woooop

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -31,14 +31,14 @@ const Sidebar = ({ collapsed, toggled, handleToggleSidebar }) => {
 
       <SidebarContent style={{ background: "#525f64", color: "#FFF" }}>
         <Menu>
-          <MenuItem icon={<MdHealthAndSafety style={{fontSize: '24px'}}/>}><Link to="/physical-safety" />Physical Safety</MenuItem>
-          <MenuItem icon={<AiFillSecurityScan style={{fontSize: '24px'}}/>}><Link to="/information-security" />Information Security</MenuItem>
           <MenuItem icon={<AiOutlineSafetyCertificate style={{fontSize: '24px'}}/>}><Link to="/digital-safety" />Digital Safety</MenuItem>
+          <MenuItem icon={<MdHealthAndSafety style={{fontSize: '24px'}}/>}><Link to="/physical-safety" />Physical Safety</MenuItem>
           <MenuItem icon={<MdPerson style={{fontSize: '24px'}}/>}>Protected Characteristics<Link to="/protected-characteristics" /></MenuItem>
+          <MenuItem icon={<AiFillSecurityScan style={{fontSize: '24px'}}/>}><Link to="/information-security" />Information Security</MenuItem>
           <MenuItem icon={<FaHouseUser style={{fontSize: '24px'}}/>}><Link to="/lawfare" />Lawfare/SLAPPS</MenuItem>
-          <MenuItem icon={<FaSmile style={{fontSize: '24px'}}/>}><Link to="/workplace-safety" />Workplace Safety</MenuItem>
           <MenuItem icon={<FaPaperclip style={{fontSize: '24px'}}/>}><Link to="/right-to-report" />Right to Report</MenuItem>
           <MenuItem icon={<FaHeart style={{fontSize: '24px'}}/>}><Link to="/mental-health" />Mental Health</MenuItem>
+          <MenuItem icon={<FaSmile style={{fontSize: '24px'}}/>}><Link to="/workplace-safety" />Workplace Safety</MenuItem>
           <SubMenu icon={<FaPaperPlane  style={{fontSize: '24px'}}/>} title="Resources"><Link to="/resources" />
             <MenuItem>Employer Responsibilities<Link to="/resources" /></MenuItem>
             <MenuItem>Equality<Link to="/resources" /></MenuItem>
